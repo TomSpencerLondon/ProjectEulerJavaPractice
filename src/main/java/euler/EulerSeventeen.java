904153188c7a3e365130fe037582aebbe7bc1d43
Tom <thomas.spencer@codurance.com>
2021-01-01 14:13:49 +0000

    Add helper methods

--- src/main/java/euler/EulerSeventeen.java
+++ src/main/java/euler/EulerSeventeen.java
@@ -19,13 +19,13 @@ public class EulerSeventeen {
             "zeros", "tens", "twenty", "thirty", "forty", "fifty",
             "sixty", "seventy", "eighty", "ninety"));
 
-    if (number >= 0 && number < 20) {
+    if (betweenOneAndTwenty(number)) {
       return ones.get(number);
-    } else if ((number >= 20 && number <= 90) && number % 10 == 0) {
+    } else if (isTensBetweenTwentyAndNinety(number, 20, 90, 10)) {
       return tens.get(number / 10);
-    } else if (number > 20 && number < 100) {
-      return tens.get(number / 10) + "-" + ones.get(number % 10);
-    } else if ((number >= 100 && number <= 900) && (number % 100 == 0)) {
+    } else if (isOnesBetweenTwentyAndHundred(number)) {
+      return tens.get(number / 10) + " " + ones.get(number % 10);
+    } else if (isHundredUpToNineHundred(number)) {
       return ones.get(number / 100) + " hundred";
     } else if (number > 100 && number < 1000) {
       return ones.get(number / 100) + " hundred and " + numberToEnglish(number % 100);
@@ -36,12 +36,27 @@ public class EulerSeventeen {
     }
   }
 
+  private static boolean isHundredUpToNineHundred(int number) {
+    return (number >= 100 && number <= 900) && (number % 100 == 0);
+  }
+
+  private static boolean isOnesBetweenTwentyAndHundred(int number) {
+    return number > 20 && number < 100;
+  }
+
+  private static boolean isTensBetweenTwentyAndNinety(int number, int i, int i2, int i3) {
+    return (number >= i && number <= i2) && number % i3 == 0;
+  }
+
+  private static boolean betweenOneAndTwenty(int number) {
+    return number >= 0 && number < 20;
+  }
+
   public static int count(int target){
     int result = 0;
     for (int i = 0; i < target; i++){
       String words = numberToEnglish(i + 1)
           .replace(" ", "")
-          .replace("-", "")
           .replace("zeros", "")
           .replace("tens", "");
       result += words.length();
