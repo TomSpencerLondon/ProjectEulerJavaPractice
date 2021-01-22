e9159e877dafdc81e06023b5ddf8d58b1667994e
Tom <thomas.spencer@codurance.com>
2021-01-22 00:34:17 +0000

    add

--- src/main/java/euler/EulerEighteen.java
+++ src/main/java/euler/EulerEighteen.java
@@ -8,4 +8,14 @@ public class EulerEighteen {
       return triangle[i][j] + Math.max(topDown(triangle, i+1, j), topDown(triangle, i+1, j+1));
     }
   }
+
+  public static void bottomUpSum(int[][] triangle){
+
+    for(int i = triangle.length-2; i>=0; i--){
+      for(int j = 0; j <= i; j++){
+
+        triangle[i][j] += Math.max(triangle [i+1][j], triangle [i+1][j+1]);
+      }
+    }
+  }
 }
